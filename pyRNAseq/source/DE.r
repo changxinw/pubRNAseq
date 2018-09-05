21d6b2ff5d91a0b9dc23003166136b164c38dbf2
Changson One <792182407@qq.com>
2018-09-05 16:23:13 +0800

    Output gene symbol as index

--- pyRNAseq/source/DE.r
+++ pyRNAseq/source/DE.r
@@ -2,10 +2,12 @@ library(tximport)
 library(readr)
 library(DESeq2)
 library(rjson)
+library(dplyr)
 
 args=commandArgs(T)
 design_path = args[1] # Path of design matrix
 output = args[2] # Path of output file
+ref = args[3] # Directory of reference file
 
 design_mat = read.table(design_path, sep="\t", header=F)
 gsm = as.vector(design_mat[,1])
@@ -16,9 +18,19 @@ files = sapply(gsm, function(i){paste0(output, i, '/quant.sf')})
 # condition = c('Ctrl', 'Ctrl', 'Treat', 'Treat')
 names = names(gsm)
 sampleTable <- data.frame(sampleName = files, fileName = files, condition = condition)
-txi_tx <- tximport(files, type="salmon", ,ignoreTxVersion = TRUE, txOut = TRUE, countsFromAbundance = "scaledTPM")
+txi_tx <- tximport(files, type="salmon", ,ignoreTxVersion = TRUE, txOut = TRUE, countsFromAbundance = "scaledTPM", dropInfReps = TRUE)
 dds_tx <- DESeqDataSetFromTximport(txi_tx,colData=sampleTable,design=~condition)
 dds_tx <- dds_tx[rowSums(counts(dds_tx)) > 1, ]
 dds_tx <- DESeq(dds_tx,fitType="local")
 res_tx <- results(dds_tx, alpha = 0.01)
-write.table(res_tx, paste0(output, '/DE_table.txt'), sep='\t', quote=F)
+de_new = na.omit(res_tx)
+refseq =read.table(ref, sep = "\t", row.names = 1)
+
+co_index = intersect(rownames(de_new), rownames(refseq))
+result = cbind(de_new[co_index,], refseq[co_index,"V6"])
+colnames(result)  = "symbol"
+result = result[order(result[,6]),]
+result2 = distinct(result, symbol, .keep_all = TRUE)
+rownames(result2) = result2$symbol
+
+write.table(result2[,1:6], paste0(output, '/DE_table.csv'), sep='\t', quote=F)
